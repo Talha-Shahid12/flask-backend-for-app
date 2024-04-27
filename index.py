83c1f6bdd12da7bb11a06f9634540a0b7142861a
talha-shahid12 <talhashahidarain@gmail.com>
2024-04-28 03:52:59 +0500

    cecw

--- index.py
+++ index.py
@@ -13,6 +13,7 @@ def calculate_sum():
 
     result = number1 + number2
     return jsonify({'result': result}), 200
-
+def home():
+    return '/'
 if __name__ == '__main__':
     app.run(debug=True)
