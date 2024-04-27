44b6de136d64531e0f37f525c3597ca5cbf66c76
talha-shahid12 <talhashahidarain@gmail.com>
2024-04-28 03:46:13 +0500

    cecw

--- index.py
+++ index.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 @app.route('/calculate_sum', methods=['POST'])
 def calculate_sum():
     data = request.json
-    num1 = int(data['num1'])
-    num2 = int(data['num2'])
-    result = num1 + num2
-    return jsonify({'result': result})
+    number1 = data.get('number1')
+    number2 = data.get('number2')
 
-@app.route('/', methods = ['GET'])
-def done():
-    return 'Hello world'
+    if number1 is None or number2 is None:
+        return jsonify({'error': 'Invalid numbers provided'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    result = number1 + number2
+    return jsonify({'result': result}), 200
+
